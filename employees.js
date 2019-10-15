b82047d21d86953f22755657bbc9c916d0d5c47b
EDGART <edgar@getpoints.com>
2019-10-15 13:38:55 +0300

    edited readme file and added db.json file

--- employees.js
+++ employees.js
@@ -8,13 +8,25 @@ function generateEmployees(){
     for(var id = 0; id = 100; id++){
         var firstName = faker.name.firstName()
         var lastName = faker.name.lastName()
+        var jobTitle = faker.name.jobTitle()
+        var jobDescriptor = faker.name.jobDescriptor()
         var email = faker.internet.email()
+        var phone = faker.phone.phoneNumber()
+        var dateOfBirth = faker.date.past()
+        var address = faker.address.zipCode()
+        var image = faker.image.avatar()
 
         employees.push({
             "id" : id,
             "first_name" : firstName,
             "last_name" : lastName,
-            "email" : email
+            "job_title" : jobTitle,
+            "email" : email,
+            "job_descriptor" : jobDescriptor,
+            "phone" : phone,
+            "date_of_birth" : dateOfBirth,
+            "address" : address,
+            "photo" : image
         })
 
         
