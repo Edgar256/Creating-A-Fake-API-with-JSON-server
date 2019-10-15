9522b48c48e1cec6b843d6e77528a2bf5a9a6959
edgar256 <tinka.edgar@gmail.com>
2019-10-15 20:03:38 +0300

    error fix

--- employees.js
+++ employees.js
@@ -1,37 +1,20 @@
-//using faker to get dummy data to play with
-
 var faker = require('faker');
 
-function generateEmployees(){
+function getEmployees(){
     var employees = []
 
-    for(var id = 0; id = 100; id++){
+    for(var id = 0; id < 100; id++){
         var firstName = faker.name.firstName()
         var lastName = faker.name.lastName()
-        var jobTitle = faker.name.jobTitle()
-        var jobDescriptor = faker.name.jobDescriptor()
-        var email = faker.internet.email()
         var phone = faker.phone.phoneNumber()
-        var dateOfBirth = faker.date.past()
-        var address = faker.address.zipCode()
-        var image = faker.image.avatar()
 
         employees.push({
-            "id" : id,
             "first_name" : firstName,
             "last_name" : lastName,
-            "job_title" : jobTitle,
-            "email" : email,
-            "job_descriptor" : jobDescriptor,
-            "phone" : phone,
-            "date_of_birth" : dateOfBirth,
-            "address" : address,
-            "photo" : image
+            "phone" : phone
         })
-
-        
     }
-    return{"employees": employees}
+    return {"employees" : employees}
 }
 
-module.exports = generateEmployees;
+module.exports = getEmployees;
